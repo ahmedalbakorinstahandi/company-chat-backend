c55088306b8a8225ca5892715928a5d5d5333941
ahmed-albakor <ahmed.albakor206@gmail.com>
2025-06-18 14:10:02 +0300

    Test

--- app/Services/PusherService.php
+++ app/Services/PusherService.php
@@ -11,15 +11,23 @@ class PusherService
     public function __construct()
     {
         $this->pusher = new Pusher(
-            env('PUSHER_APP_KEY'),
-            env('PUSHER_APP_SECRET'), 
-            env('PUSHER_APP_ID'),
+            env('PUSHER_APP_KEY', 'aad0418787f85ad833f7'),
+            env('PUSHER_APP_SECRET', 'd82e595a10197a5be4a4'), 
+            env('PUSHER_APP_ID', '2007760'),
             [
-                'cluster' => env('PUSHER_APP_CLUSTER'),
+                'cluster' => env('PUSHER_APP_CLUSTER', 'us3'),
                 'useTLS' => true,
             ]
         );
     }
+
+//     PUSHER_APP_ID="2007760"
+// PUSHER_APP_KEY="aad0418787f85ad833f7"
+// PUSHER_APP_SECRET="d82e595a10197a5be4a4"
+// PUSHER_HOST=
+// PUSHER_PORT=443
+// PUSHER_SCHEME=https
+// PUSHER_APP_CLUSTER="us3"
 
     public function sendMessage($channel, $event, $data)
     {
